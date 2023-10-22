cc5f70551420f767e4eead35190c7c82dd24d86c
Jon Sagara <jon@sagarasoftware.com>
2023-10-22 15:08:05 -0700

    Check arguments.

--- src/PollyCoreConsole/PollyHelper.cs
+++ src/PollyCoreConsole/PollyHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Http.Resilience;
 using Polly;
 using Polly.Retry;
+using Sagara.Core;
 using Serilog;
 
 namespace PollyCoreConsole;
@@ -11,12 +12,15 @@ public static class PollyHelper
 
     public static void ConfigureRetryAndWaitWithExponentialBackoffStrategy(ResiliencePipelineBuilder<HttpResponseMessage> pipelineBuilder, int maxRetryAttempts, string? httpClientName)
     {
+        Check.NotNull(pipelineBuilder);
+        Check.NotOutOfRange(maxRetryAttempts, rangeLo: 1, rangeHi: int.MaxValue);
+
         pipelineBuilder.AddRetry(new RetryStrategyOptions<HttpResponseMessage>()
         {
             ShouldHandle = args => ValueTask.FromResult(HttpClientResiliencePredicates.IsTransientHttpOutcome(args.Outcome)),
             MaxRetryAttempts = maxRetryAttempts,
             BackoffType = DelayBackoffType.Exponential,
-            OnRetry = (args) =>
+            OnRetry = args =>
             {
                 _logger.Warning(args.Outcome.Exception, "[HttpClient={HttpClientName}] Failed to send request to {RequestUri}. StatusCode: {StatusCodeInt} {StatusCode}. The attempt took {Duration}. Retrying after {RetryDelay}...",
                     httpClientName,
